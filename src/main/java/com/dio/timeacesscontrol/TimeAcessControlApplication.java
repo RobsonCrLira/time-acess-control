a100d7e7da1297dc4065b66166fa46390855ba25
Robson Cristian de Lira <robso.crlira@gmail.com>
2021-08-07 16:05:31 -0300

    Iniciando o Projeto

---
+++ src/main/java/com/dio/timeacesscontrol/TimeAcessControlApplication.java
@@ -0,0 +1,13 @@
+package com.dio.timeacesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeAcessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeAcessControlApplication.class, args);
+	}
+
+}
